64804c6e16f33eed8d317d083ff3187041f90257
Vlad Samoilov <p0lonium84@yandex.ru>
2024-10-13 14:42:37 +0700

    Add changes to TimeSet() function, add comments to code, write file README.docx

--- main.c
+++ main.c
@@ -1,29 +1,35 @@
 #include "stm32f10x.h"
 #include "GPIO_STM32F10x.h"
 
-#define INC_BTN 		4
-#define LED2 			5
-#define CLOCKTIME_BTN 	6
-#define ALARMTIME_BTN 	7
+#define INC_BTN 		4	// Кнопка увеличения изменяемого параметра (минуты или секунды) на 1, также используется для отключения сигнала сработавшего будильника
+#define LED2 			5	// Светодиод для подачи сигнала (светодиод загорается при срабатывании будильника)
+#define CLOCKTIME_BTN 	6	// Кнопка перехода в режим настройки часов (текущего времени) ИЛИ кнопка подтверждения установленных минут или часов в этом режиме
+#define ALARMTIME_BTN 	7	// Кнопка перехода в режим настройки будильника ИЛИ кнопка подтверждения установленных минут или часов в этом режиме
 
-typedef struct time_tag{
+typedef struct time_tag{	// Структура для хранения времени часов и будильника
 	uint8_t seconds;
 	uint8_t minutes;
 	uint8_t hours;
 } time;
 
-static uint64_t TIM3_interrupts;
-static uint8_t clockTimeSetting;
-static uint8_t alarmTimeSetting;
-static uint8_t clockTimeBtnClick;
-static uint8_t alarmTimeBtnClick;
-static uint8_t incrementBtnClick;
-static uint8_t alarmIsOn;
-static uint8_t mutex;
-static uint8_t alarmSignal;
-static uint8_t mode;
-static time currentTime, alarmTime;
+static uint64_t TIM3_interrupts;	// Счетчик прерываний таймера для счета секунд
 
+/* 
+*	Флаги режимов работы
+*	По умолчанию принимают нулевые значения
+*/
+static uint8_t clockTimeSetting;	// Флаг режима настройки часов
+static uint8_t alarmTimeSetting;	// Флаг режима настройки будильника
+static uint8_t clockTimeBtnClick;	// Флаг нажатия на кнопку настройки часов
+static uint8_t alarmTimeBtnClick;	// флаг нажатия на кнопку настройки будильника
+static uint8_t incrementBtnClick;	// Флаг нажатия на кнопку инкремента 
+static uint8_t alarmIsOn;			// Флаг включения режима дежурства (1 - будильник включен, 0 - будильник отключен); защищает от непреднамеренной подачи сигнала тревоги
+static uint8_t alarmSignal;			// Флаг сигнала тревоги (1 - светодиод горит, 0 - светодиод не горит)
+static uint8_t mode;				// Флаг режима настройки времени (0 - настройка минут, 1 - настройка часов, 2 - настройка завершена)
+
+static time currentTime, alarmTime;	// Экземпляры времени часов и будильника
+
+/* Настройка тактирования */
 void SystemCoreClockConfigure(void) {
 	RCC->CR |= ((uint32_t)RCC_CR_HSEBYP);                    // Включение HSE 
 	while ((RCC->CR & RCC_CR_HSERDY) == 0);                  
@@ -48,9 +54,10 @@ void SystemCoreClockConfigure(void) {
 	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL);  
 }
 
+/* Настройка таймера TIM3 (прерывание срабатывает каждую секунду) */
 void TIM3_Init() {															 
-	RCC -> APB1ENR |= RCC_APB1ENR_TIM3EN; 				 	 // Настройка таймера TIM3 (прерывание срабатывает каждую секунду)
-	TIM3 -> CR1 = TIM_CR1_CEN;										 
+	RCC -> APB1ENR |= RCC_APB1ENR_TIM3EN; 				 	 // Разрешение тактирования
+	TIM3 -> CR1 = TIM_CR1_CEN;								 // Включение счетчика
 
 	TIM3->PSC = (uint16_t)(SystemCoreClock / 1000 - 1);	     // Определение значений предделителя (длительность одного тика таймера) 
 	TIM3->ARR = 1000 - 1;									 // и регистра автоматичекой перезагрузки (количество тиков)
@@ -59,41 +66,40 @@ void TIM3_Init() {
 	NVIC_EnableIRQ (TIM3_IRQn);										 
 }
 
+/* Настройка порта ввода-вывода*/
 void GPIO_Init(void) {            		 	 
-	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; // Настройка порта GPIO 
+	RCC->APB2ENR |= RCC_APB2ENR_IOPAEN; 					 						// Включение тактирования порта GPIO 
 	
-	GPIO_PinConfigure(GPIOA, LED2, GPIO_OUT_PUSH_PULL, GPIO_MODE_OUT10MHZ); // Настройка подачи сигнала на светодиод LD2 (порт PA5)
+	GPIO_PinConfigure(GPIOA, LED2, GPIO_OUT_PUSH_PULL, GPIO_MODE_OUT10MHZ); 		// Настройка подачи сигнала на светодиод LD2 (порт PA5)
 	
-	GPIO_PinConfigure(GPIOA, INC_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); // Настройка подачи сигнала на порт PA4 (кнопка прибавления единицы к настраиваемому параметру (минуты или часы))
-	GPIO_PinConfigure(GPIOB, CLOCKTIME_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); // Настройка подачи сигнала на порт PA6 (кнопка настройки текущего времени)
-	GPIO_PinConfigure(GPIOC, ALARMTIME_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); // Настройка подачи сигнала на порт PA7 (кнопка настройки времени срабатывания будильника)
-
-//	GPIOA->CRL &= ~(15ul << 4 * LED2);              	 
-//	GPIOA->CRL |=  (1ul << 4 * LED2);
+	GPIO_PinConfigure(GPIOA, INC_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); 			// Настройка подачи сигнала на порт PA4 (кнопка прибавления единицы к настраиваемому параметру (минуты или часы))
+	GPIO_PinConfigure(GPIOB, CLOCKTIME_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); 	// Настройка подачи сигнала на порт PA6 (кнопка настройки текущего времени)
+	GPIO_PinConfigure(GPIOC, ALARMTIME_BTN, GPIO_IN_PULL_DOWN, GPIO_MODE_INPUT); 	// Настройка подачи сигнала на порт PA7 (кнопка настройки времени срабатывания будильника)
 }
 
+/* Настройка внешних прерываний */
 void NVIC_InputInit()
 {
-	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
+	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;															// Включение тактирования альтернативных функций
 	
-	AFIO->EXTICR[1] |= AFIO_EXTICR2_EXTI4_PA | AFIO_EXTICR2_EXTI6_PA | AFIO_EXTICR2_EXTI7_PA; // interrupt source
-	EXTI->IMR |= EXTI_IMR_MR4 | EXTI_IMR_MR6 | EXTI_IMR_MR7;
-	EXTI->RTSR |= EXTI_RTSR_TR4 | EXTI_RTSR_TR6 | EXTI_RTSR_TR7;
-	NVIC_EnableIRQ(EXTI4_IRQn);
+	AFIO->EXTICR[1] |= AFIO_EXTICR2_EXTI4_PA | AFIO_EXTICR2_EXTI6_PA | AFIO_EXTICR2_EXTI7_PA;   // Выбор источников прерывания (PA4, PA6, PA7)
+	EXTI->IMR |= EXTI_IMR_MR4 | EXTI_IMR_MR6 | EXTI_IMR_MR7;									// Разрешение генерации прерываний в периферии
+	EXTI->RTSR |= EXTI_RTSR_TR4 | EXTI_RTSR_TR6 | EXTI_RTSR_TR7;								// Настройка детектирования прерывания по восходящему фронту
+	NVIC_EnableIRQ(EXTI4_IRQn);																	// Разрешение прерывания в NVIC
 	NVIC_EnableIRQ(EXTI9_5_IRQn);
 }
 
 /* Включение светодиода */
 void ALARM_ON() {
-	alarmSignal = 1;
-	GPIOA->BSRR = 1ul << LED2;                                  
+	alarmSignal = 1;			// Начало подачи сигнала тревоги
+	GPIOA->BSRR = 1ul << LED2;  // Зажигание светодиода                            
 }
 
 /* Выключение светодиода */
 void ALARM_OFF() {
-	alarmSignal = 0;
-	alarmIsOn = 0;	
-	GPIOA->BSRR = 1ul << (LED2 + 16);
+	alarmSignal = 0;					// Завершение подачи сигнала тревоги
+	alarmIsOn = 0;	            		// Отключение режима дежурства будильника (при совпадении времени на следующие сутки будильник не сработает, после срабатывания его нужно заводить заново)
+	GPIOA->BSRR = 1ul << (LED2 + 16); 	// Потухание светодиода
 }
 
 /* Настройка обработчика прерываний для TIM3 */
@@ -101,141 +107,134 @@ void TIM3_IRQHandler() {
 	TIM3->SR &= ~TIM_SR_UIF;												// Снятие флага события обновления
 	TIM3_interrupts++;														// Увеличение счетчика прерываний
 	
-	if(!clockTimeSetting)
+	if(!clockTimeSetting)													// Изменение времени в структуре в режиме настройки часов запрещено
 	{
-		currentTime.hours = (uint8_t)(TIM3_interrupts % 86400 / 3600);
+		currentTime.hours = (uint8_t)(TIM3_interrupts % 86400 / 3600);		// Вычисление часов, минут, секунд текущего времени
 		currentTime.minutes = (uint8_t)(TIM3_interrupts % 3600 / 60);
 		currentTime.seconds = (uint8_t)(TIM3_interrupts % 60);
 	}
 }
 
+/* 
+*	Обработка нажатия кнопки инкремента 
+*	Нажатие кнопки фиксируется в следующих ситуациях:
+*	1) в режимах настройки часов или будильника (для настройки минут или часов)
+*	2) при подаче сигнала тревоги (для его выключения)
+*   В обычном режиме работы нажатие игнорируется
+*/
 void EXTI4_IRQHandler(void)
 {
-	incrementBtnClick = (!mutex) & (clockTimeSetting | alarmTimeSetting | alarmSignal);
-	EXTI->PR |= EXTI_PR_PR4; // Очищаем флаг
+	incrementBtnClick = clockTimeSetting | alarmTimeSetting | alarmSignal; 
+	EXTI->PR |= EXTI_PR_PR4;	// Очистка флага
 }
 
+/* Обработка нажатия кнопок настройки часов и будильника */
 void EXTI9_5_IRQHandler(void)
 {
-	if(!mutex) 
-	{
-		if (EXTI->PR & (1ul << CLOCKTIME_BTN)) //проверяем прерывание от EXTI6
-		{ 
-			clockTimeBtnClick = 1;
-			EXTI->PR |= (1ul << CLOCKTIME_BTN);  // Очищаем флаг
-		}
-		  
-		if (EXTI->PR & (1ul << ALARMTIME_BTN)) //проверяем прерывание от EXTI6
-		{ 
-			alarmTimeBtnClick = 1;
-			EXTI->PR |= (1ul << ALARMTIME_BTN);  // Очищаем флаг
-		}
-	}
+	clockTimeBtnClick = (EXTI->PR & (1ul << CLOCKTIME_BTN)); // Проверка срабатывания прерывания								
+	alarmTimeBtnClick = (EXTI->PR & (1ul << ALARMTIME_BTN)); // Проверка срабатывания прерывания
+	
+	EXTI->PR |= (1ul << CLOCKTIME_BTN);						 // Очистка флагов
+	EXTI->PR |= (1ul << ALARMTIME_BTN);  								
 }
 
+/* 
+*	Функция сравнения времени 
+*	Возвращает 1 при совпадени времени будильника и часов, в противном случае - 0
+*/
 uint8_t compareTime(time *time_1, time *time_2)
 {
 	return (time_1->hours == time_2->hours) && (time_1->minutes == time_2->minutes);
 }
 
-void TimeSet(time *p_time, uint8_t *buttonClick)
+/*
+*	Функция настройки времени для часов или будильника
+*	Тип настраиваемого устройства (часы или будильник) определяется через указатель на кнопку p_buttonClick
+*/
+void TimeSet(time *p_time, uint8_t *p_buttonClick)
 {
-	p_time->hours = 0;
+	p_time->hours = 0;		// Сброс времени
 	p_time->minutes = 0;
 	
-	mode = 0;
-	*buttonClick = 0;
+	mode = 0;				// Установка режима настройки минут
+	*p_buttonClick = 0;		// Обнуление нажатия кнопки настройки после перехода в режим настройки
 	while(1)
 	{
-		switch(mode)
+		switch(mode)		
 		{
-			case 0:
+			case 0:			// Режим настройки минут
 			{
-				if(incrementBtnClick)
+				if(incrementBtnClick) 								// При нажатии кнопки инкремента
 				{
-					mutex = 1;
-					{
-						p_time->minutes = (p_time->minutes + 1) % 60;
-						incrementBtnClick = 0;
-					}
-					mutex = 0;
+					p_time->minutes = (p_time->minutes + 1) % 60;	// Увеличивается на 1 количество минут (диапазон: 0...59)
+					incrementBtnClick = 0;							// Обнуляется флаг нажатия кнопки инкремента (клик считается обработанным)
 				}
 				
-				if(*buttonClick)
+				if(*p_buttonClick)									// При нажатии кнопки настройки устройства
 				{
-					mutex = 1;
-					{
-						mode = 1;
-						*buttonClick = 0;
-					}
-					mutex = 0;
+					mode++;											// Происходит переход в следующий режим (в режим настройки часов)
+					*p_buttonClick = 0;								// Обнуляется флаг нажатия кнопки настройки устройства (клик считается обработанным)
 				}
 			}
 				break;
-			case 1:
+			case 1:			// Режим настройки часов
 			{
-				if(incrementBtnClick)
+				if(incrementBtnClick)								// При нажатии кнопки инкремента
 				{
-					mutex = 1;
-					{
-						p_time->hours = (p_time->hours + 1) % 24;
-						incrementBtnClick = 0;
-					}
-					mutex = 0;
+					p_time->hours = (p_time->hours + 1) % 24;		// Увеличивается количество часов (диапазон: 0...23)
+					incrementBtnClick = 0;							// Обнуляется флаг нажатия кнопки инкремента (клик считается обработанным)
 				}
 				
-				if(*buttonClick)
+				if(*p_buttonClick)									// При нажатии кнопки настройки устройства
 				{
-					mutex = 1;
-					{
-						mode = 2;
-						*buttonClick = 0;
-					}
-					mutex = 0;
+					mode++;											// Происходит переход в следующий режим (в режим завершения настройки)
+					*p_buttonClick = 0;								// Обнуляется флаг нажатия кнопки настройки устройства (клик считается обработанным)
 				}
 			}
 				break;
-			default: 
+			default:        // Режим завершения настройки
 			{
-				return;
+				return;		// Выход из функции
 			}
 		}
 	}
 }
 
 int main (void){
-	SystemCoreClockConfigure();                             
-	SystemCoreClockUpdate();
+	SystemCoreClockConfigure();     // Настройка тактирования                        
+	SystemCoreClockUpdate();		// Обновление частоты
 			
-	GPIO_Init();
+	GPIO_Init();					// Инициализация порта ввода-вывода, таймера и внешних прерываний
 	TIM3_Init();
 	NVIC_InputInit();
 	while (1) 
 	{
+		/* Сигнал тревоги подается, если до этого он был выключен, будильник стоит на дежурстве и совпало время часов и будильника */
 		if(!alarmSignal && alarmIsOn && compareTime(&currentTime, &alarmTime))
 		{
 			ALARM_ON();
 		}
 		
-		if(alarmSignal && incrementBtnClick && !clockTimeSetting && !alarmTimeSetting)
+		/* Сигнал тревоги отключается, если до этого он был включен, нажата кнопка инкремента и устройство не находится в режиме настройки часов и будильника */
+		if(alarmSignal && incrementBtnClick)
 		{
 			ALARM_OFF();
 		}
 		
-		if(clockTimeBtnClick)
+		if(clockTimeBtnClick)														// При нажатии кнопки настройки часов
 		{
-			clockTimeSetting = 1;
-			TimeSet(&currentTime, &clockTimeBtnClick);
-			TIM3_interrupts = currentTime.hours * 3600 + currentTime.minutes * 60;
-			clockTimeSetting = 0;
+			clockTimeSetting = 1;													// Устанавливается режим настройки часов
+			TimeSet(&currentTime, &clockTimeBtnClick);								// Производится вызов функции настройки
+			TIM3_interrupts = currentTime.hours * 3600 + currentTime.minutes * 60;  // После завершения настройки обновляется общее время на таймере
+			clockTimeSetting = 0;													// Происходит возврат в обычный режим работы
 		}
 		
-		if(alarmTimeBtnClick)
+		if(alarmTimeBtnClick)														// При нажатии кнопки настройки будильника
 		{
-			alarmTimeSetting = 1;
-			TimeSet(&alarmTime, &alarmTimeBtnClick);
-			alarmIsOn = 1;
-			alarmTimeSetting = 0;
+			alarmTimeSetting = 1;													// Устанавливается режим настройки будильника
+			TimeSet(&alarmTime, &alarmTimeBtnClick);								// Производится вызов функции настройки 
+			alarmIsOn = 1;															// После завершения настройки будильник ставится на дежурство
+			alarmTimeSetting = 0;													// Происходит возврат в обычный режим работы
 		}
 	}
 }
